9e904138dd54c2608b2468c24c5a8890e7c8b488
Praveen K <praveenkrishna0512@gmail.com>
2023-07-30 12:48:51 +0000

    final commit

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-# 4catalyzer-hackathon
-
-# TODO: 
-- Document the installation of the diffusers package required
-- Upload best checkpoints
-=======
 # [Lesionize](https://lesionize.io) - Synthetic Stroke Lesion Generation for MRI Data
 
 [![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https://github.com/Nutlope/strokeGPT&env=REPLICATE_API_KEY&project-name=stroke-GPT&repo-name=strokeGPT)
@@ -13,9 +6,32 @@
 
 ## How it works
 
-StrokeGPT utilizes a latent diffusion model to generate synthetic stroke lesions that mimic those found in ischemic stroke patient MRI scans. This functionality is specifically designed to assist stroke detection and segmentation projects by providing augmented data for training and improving stroke detection models.
+Lesionize utilizes a latent diffusion model to generate synthetic stroke lesions that mimic those found in ischemic stroke patient MRI scans. This functionality is specifically designed to assist stroke detection and segmentation projects by providing augmented data for training and improving stroke detection models.
 
 The application allows users to upload an image of an MRI scan. The uploaded images are then processed using the latent diffusion model through a Next.js API route, and the generated image with synthetic stroke lesions is returned to the user. The latent diffusion model is hosted on [Replicate](https://replicate.com), and [Upload](https://upload.io) is used for image storage.
+
+### Datasets procured
+
+For the purposes of this application, MRI DWI Images of the axial plane were gathered from two main datasets
+1) [ISLES22 Grand Challenge Dataset](https://isles22.grand-challenge.org/dataset/) -- This dataset contains MRI DWI images of the brains of stroke patients. The lesions within the brain have been annotated on the MRI images.
+2) [OpenNeuro NIMH Healthy Research Volunteer Dataset](https://openneuro.org/datasets/ds004215/versions/1.0.0) -- This dataset contains MRI DWI images of the brains of healthy patients.
+
+The data was processed and cleaned to allow Hugging Face to curate our custom dataset using ImageFolder.
+
+### Training Approach
+
+We utilized the [InstructPix2Pix](https://huggingface.co/docs/diffusers/training/instructpix2pix) training approach and pipeline for our use case.
+
+This pipeline takes in an input image and prompt, and outputs the input image edited according to the prompt's specifications.
+
+Thus, each training data object consisted of the following data:
+- input image: the MRI DWI scan of the brain of a healthy patient
+- prompt: a prompt describing the type of lesions to insert into the MRI scan of the healthy brain
+- edited image: the MRI DWI scan of the brain of a patient with stroke lesions
+
+Thus, we allow the model to pick up "instructions" from the prompt, to amend the input image.
+
+The training script can be found within the diffusers module, or by clicking [here](https://github.com/huggingface/diffusers/blob/aae27262f408957ff53c64b3a18581959e8fd8e0/src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_instruct_pix2pix.py#L139)
 
 ## Running Locally
 
@@ -38,4 +54,3 @@ npm run dev
 Deploy the example using [Vercel](https://vercel.com?utm_source=github&utm_medium=readme&utm_campaign=vercel-examples):
 
 [![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https://github.com/Nutlope/strokeGPT&env=REPLICATE_API_KEY&project-name=stroke-GPT&repo-name=strokeGPT)
->>>>>>> 92871781360e793642c02f3a0c05ea0028256a7b
--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,31 @@ from typing import Union
 
 from fastapi import FastAPI
 
+import requests
+import torch
+from PIL import Image
+from io import BytesIO
+import os
+from diffusers import StableDiffusionInstructPix2PixPipeline
+import io
+
+
 app = FastAPI()
+
+# model stuff
+device = "cuda"
+# model_id_or_path = "runwayml/stable-diffusion-v1-5"
+model_id_or_path = "../opt/run-overnight"
+pipe = StableDiffusionInstructPix2PixPipeline.from_pretrained(model_id_or_path, torch_dtype=torch.float16, low_cpu_mem_usage=False, ignore_mismatched_sizes=True)
+pipe = pipe.to(device)
+
+prompts = {
+    "h2h": "This is an image of a normal brain MRI scan from axial view. It should be grey scale and clearly show brain structure. Do not modify the input image.",
+    "h2uh": "Given an input image of a normal brain MRI scan from axial view, modify the image by inserting a brain lesion within the MRI image. This brain lesion is caused by an ischaemic stroke. The lesion should appear relatively lighter compared to its surroundings brain tissue in the image.",
+    "uh2uhm": "Given an input image of an unhealthy brain MRI scan from axial view that contains brain lesions, modify the image to colour the lesions light red.",
+    "h2uhm": "Given an input image of a normal brain MRI scan from axial view, modify the image by inserting a brain lesion within the MRI image, and colour the lesions light red. This brain lesion is caused by an ischaemic stroke.",
+    "uhm2uhm": "This is an image of an unhealthy brain MRI scan from axial view that contains brain lesions. The lesions are coloured light red."
+}
 
 
 @app.get("/")
@@ -11,5 +35,44 @@ def read_root():
 
 
 @app.get("/generate")
-async def generate(image_url: str):
-    return {"image_url": image_url}
+async def generate(num_inference_steps: int, image_guidance_scale: float, guidance_scale: float, image_url: str,):
+    generated_urls = infer(image_url, num_inference_steps, image_guidance_scale, guidance_scale)
+    return {"generated_urls": generated_urls}
+
+
+
+def infer(image_url, num_inference_steps, image_guidance_scale, guidance_scale):
+    response = requests.get(image_url)
+    response.raise_for_status()
+    init_image = Image.open(io.BytesIO(response.content)).convert("RGB")
+
+    prompt = "modify the image by inserting brain lesions within the MRI image, and highlight the brain lesions in bright red color."
+    print("generating")
+    generator = torch.Generator("cuda").manual_seed(40)
+        
+    images = pipe(prompt, image=init_image,
+                num_inference_steps = num_inference_steps, #int
+                image_guidance_scale = image_guidance_scale, #float
+                guidance_scale = guidance_scale, #float
+                num_images_per_prompt=1,
+                generator=generator).images
+
+    print("done")
+    uploaded_urls = []
+    for idx, image in enumerate(images):
+        # Convert the Pillow image to bytes (in-memory representation)
+        img_bytes = io.BytesIO()
+        image.save(img_bytes, format='JPEG')
+        img_bytes.seek(0)
+
+        cdn_upload_url = "https://api.upload.io/v2/accounts/12a1yU7/uploads/binary"
+        headers = {
+            "Authorization": "Bearer public_12a1yU77jroPKckv6W5q5GBXUpv4",
+            "Content-Type": "image/jpeg"  # Change this to match the actual image MIME type
+        }
+        cdn_response = requests.post(cdn_upload_url, headers=headers, data=img_bytes)
+        cdn_response.raise_for_status()  # Check for any errors in the CDN upload
+        uploaded_url = cdn_response.json().get("fileUrl")  # Assuming the CDN API returns the uploaded URL in the response
+        uploaded_urls.append(uploaded_url)
+
+    return uploaded_urls
